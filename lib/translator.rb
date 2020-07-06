31728b81ef7327876e554d9c905dc0e4d17c9171
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-06 20:48:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(file_path)
   emoticons.each do |english_word, emoticon_set|
     emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
     emoticon_hash["get_meaning"][emoticon_set.last] = english_word
- #binding.pry
+ binding.pry
 end
  emoticon_hash
 end
