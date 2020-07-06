8a78c86306c24b0dd35e5ce1cf2b6a8585e683ae
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-06 20:14:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,9 +6,13 @@ def load_library(file_path)
   emoticon_hash = Hash.new
   emoticon_hash["get_emoticon"] = Hash.new
   emoticon_hash["get_meaning"] = Hash.new
-
+  emoticons.each do |english_word, emoticon_set|
+    emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
+    emoticon_hash["get_meaning"][emoticon_set.last] = english_word
  #binding.pry
 end
+ emoticon_hash
+end 
 
 def get_japanese_emoticon()
   # code goes here
