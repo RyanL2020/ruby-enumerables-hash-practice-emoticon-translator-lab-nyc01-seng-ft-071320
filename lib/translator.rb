861c07aa85e2f65caecbe78092cacc269e854921
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-06 20:13:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,6 +4,9 @@ require 'yaml'# require modules here
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   emoticon_hash = Hash.new
+  emoticon_hash["get_emoticon"] = Hash.new
+  emoticon_hash["get_meaning"] = Hash.new
+
  #binding.pry
 end
 
