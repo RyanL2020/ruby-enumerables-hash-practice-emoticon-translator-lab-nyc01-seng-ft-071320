88995cd0c677d0eec8b15004d25329c0a1ca4a82
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-06 20:17:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,5 +24,10 @@ def get_japanese_emoticon(file_path, emoticon)
 end
 
 def get_english_meaning(file_path, emoticon)
-  # code goes here
+  emoticon_hash = load_library(path)
+ result = emoticon_hash["get_meaning"][emoticon]
+ if result == nil
+   result = "Sorry, that emoticon was not found"
+ end
+ result
 end
