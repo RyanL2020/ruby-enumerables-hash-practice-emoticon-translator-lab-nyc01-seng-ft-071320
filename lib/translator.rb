527e9fdafb421d83191a5bf9d08c36aa181becb9
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-06 20:55:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   emoticon_hash = Hash.new
  end
-binding.pry
+#binding.pry
 def get_japanese_emoticon(file_path, emoticon)
   emoticon_hash = load_library(file_path)
   result = emoticon_hash["get_emoticon"][emoticon]
