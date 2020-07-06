a08b0cf342cde16f221df1289ed7a4ca0f989b20
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-06 19:52:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,10 +4,10 @@ def load_library(yaml)
  emoticons = YAML.load(File.read("emoticons.yml"))
 end
 
-def get_japanese_emoticon(,)
+def get_japanese_emoticon()
   # code goes here
 end
 
-def get_english_meaning(,)
+def get_english_meaning()
   # code goes here
 end
